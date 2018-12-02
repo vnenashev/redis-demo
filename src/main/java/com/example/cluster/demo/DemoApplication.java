d66890c74e4ba36f644f12a91355cbfa9b6768ee
Vladimir Nenashev <vn@payneteasy.com>
2018-12-02 21:43:08 +0300

    updated redisson config

--- src/main/java/com/example/cluster/demo/DemoApplication.java
+++ src/main/java/com/example/cluster/demo/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.example.cluster.demo;
 
+import org.redisson.spring.session.config.EnableRedissonHttpSession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRedissonHttpSession
 public class DemoApplication {
 
     public static void main(final String[] args) {
